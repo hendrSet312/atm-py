22200c3001a434411964dd49dc5f787c9be0f591
Hendry Setiawan <72367912+hendrSet312@users.noreply.github.com>
2021-11-04 14:47:19 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 from atm import atm_machine
 from user import user
 
-#self,username,pin,accnum,usermoney,realmoney,emoney
+#self,name,pin,accnum,usermoney,realmoney,emoney
 atm1= atm_machine("record.txt",True)
-user1= user("Yogi Pangayo",123456,894327837878,70000,60000,80000)
-user2= user("Devin setiawan ",123426,24689012992,80000,90000,100000)
+#userExample= user("John Doe",123456,894327837878,70000,60000,80000)
+user1= user("{Name}","{pin}","{account id}","{bank account}","{real money}","{e-money}")
+user2= user("{Name}","{pin}","{account id}","{bank account}","{real money}","{e-money}")
 #inserting the atm-card and activate the atm machine 
 atm1.condATM=[True,True]
 #run the atm program
@@ -18,7 +19,7 @@ while atminserted=='CardInside:True':
         atm_run= atm1.selectTransaction(user1)
         if atm_run==None:
             atm1.releaseCard()
-            print("The transaction is finished, you can see the log for activities")
+            print("The transaction is finished, you can see the log file for your current activities")
             break
 
 
